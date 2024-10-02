40e5d79804ce95c18a0a88548f2e315bbaf3fe47
twelverakki <twelve.rakki@gmail.com>
2024-10-02 14:54:06 +0800

    fase 1

---
+++ NoteMaster/__init__.py
@@ -0,0 +1,9 @@
+# from .cari_catatan import cari_catatan
+# from .edit_catatan import edit_catatan
+# from .hapus_catatan import hapus_catatan
+# from .simpan_backup_catatan_6 import simpan_catatan_json, simpan_catatan_txt, simpan_catatan_csv, backup_catatan
+# from .tambah_catatan import tambah_catatan
+# from .tampilkan_catatan import tampilkan_catatan
+
+
+# ubah nama file ges, biar ini bisa di pakai ya1
