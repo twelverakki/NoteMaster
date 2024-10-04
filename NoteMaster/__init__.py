bbd1dda5e160e9c21dea22b64435fd073219c973
ShinZeleo <imamqhoir28@gmail.com>
2024-10-04 20:22:27 +0800

    init rev

--- NoteMaster/__init__.py
+++ NoteMaster/__init__.py
@@ -1,9 +1,9 @@
-# from .cari_catatan import cari_catatan
-# from .edit_catatan import edit_catatan
-# from .hapus_catatan import hapus_catatan
+from .module_1 import cari_catatan
+from .module_2 import edit_catatan
+from .module_3 import hapus_catatan
+from .module_4 import tambah_catatan
+from .module_5 import tampilkan_catatan
 from .module_6 import simpan_catatan_json, simpan_catatan_txt, simpan_catatan_csv, backup_catatan
-# from .tambah_catatan import tambah_catatan
-# from .tampilkan_catatan import tampilkan_catatan
 
 
-# ubah nama file ges, biar ini bisa di pakai ya1
+# ubah bagian setelah import... jadi nama fungsi kalian, biar ini bisa di pakai ya1
