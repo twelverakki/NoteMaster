4e3e954c661fe54f30d51f17eca537e61a6dcf4e
twelverakki <twelve.rakki@gmail.com>
2024-10-06 19:20:32 +0800

    update init dan setup

--- NoteMaster/__init__.py
+++ NoteMaster/__init__.py
@@ -1,9 +1,23 @@
-# from .cari_catatan import cari_catatan
-# from .edit_catatan import edit_catatan
-# from .hapus_catatan import hapus_catatan
-# from .simpan_backup_catatan_6 import simpan_catatan_json, simpan_catatan_txt, simpan_catatan_csv, backup_catatan
-# from .tambah_catatan import tambah_catatan
-# from .tampilkan_catatan import tampilkan_catatan
+from .module_1 import tambah_catatan, tambah_catatan_existing, view_notes
+from .module_2 import hapus_all, hapus_catatan
+from .module_3 import edit_catatan
+from .module_4 import tampilkan_catatan
+from .module_5 import cari_catatan, pencarian_catatan
+from .module_6 import simpan_catatan_json, simpan_catatan_txt, simpan_catatan_csv, backup_catatan
 
 
-# ubah nama file ges, biar ini bisa di pakai ya1
+_all_ = [
+    'tambah_catatan',
+    'tambah_catatan_existing',
+    'view_notes',
+    'hapus_all',
+    'hapus_catatan',
+    'edit_catatan',
+    'tampilkan_catatan',
+    'cari_catatan',
+    'pencarian_catatan',
+    'simpan_catatan_json',
+    'simpan_catatan_txt',
+    'simpan_catatan_csv',
+    'backup_catatan',
+]
